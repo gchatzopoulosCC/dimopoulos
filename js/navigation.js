f94074ae7cc68cefe21dc3c9f5d517cb45e8b3ab
gchatzopoulosCC <gchatzopoulos@york.citycollege.eu>
2024-01-12 14:42:05 +0200

    Finish the dropdown menu (alternative)

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,24 @@
+// Navigation variables
+let restaurants = [];
+let bars = [];
+let clubs = ["jadore", "suita", "nhaos", "casper", "aigli", "pili axiou"];
+
+// Temp
+for (let i = 1; i < 5; i++) {
+    restaurants.push(`restaurant ${i}`);
+}
+for (let i = 1; i < 6; i++) {
+    bars.push(`bar ${i}`);
+}
+
+
 // Dropdown content
 const dropdowns = document.querySelectorAll(".dropdowns");
 
 // When the user hovers over a menu display the correct content
 dropdowns.forEach(dropdown => {
     const dropdownDiv = dropdown.querySelector(".dropdown-div");
-    console.log(dropdownDiv)
+
     // On hover
     dropdown.addEventListener('mouseover', function(){
         dropdownDiv.style.display = "block";
@@ -14,10 +28,32 @@ dropdowns.forEach(dropdown => {
     dropdown.addEventListener('mouseout', function(){
         dropdownDiv.style.display = "none";
     });
+
+    // Dynamically add the content to the menu
+    const menu = dropdown.querySelector(".dropdown-menu");        
+    let data = dropdown.dataset;
+    let dropdownInfo;
+    // Find the dropdown the user hovers
+    switch(data.name) {
+        case "restaurants":
+            dropdownInfo = restaurants;
+            break;
+        case "bars":
+            dropdownInfo = bars;
+            break;
+        case "clubs":
+            dropdownInfo = clubs;
+            break;
+        default:
+            dropdownInfo = [""];
+        }
+
+        // Clear before adding content
+        menu.textContent = "";
+        dropdownContent(menu, dropdownInfo);
 });
 
 // Dropdown menu rules
-
 const dropdownDivs = document.querySelectorAll(".dropdown-div");
 dropdownDivs.forEach(div => {
     div.addEventListener('mouseover', function(){
@@ -40,3 +76,10 @@ const burgerBars = document.querySelectorAll(".burger-bars");
 burgerMenu.addEventListener('click', function(){
     burgerBars.forEach(bar => bar.classList.toggle('clicked'));
 });
+
+// Add the content when a dropdown is hovered
+function dropdownContent(menu, dropdownInfo) {
+    dropdownInfo.forEach(content => {
+        menu.innerHTML += `<li class="dropdown-menu-li">${content}</li>`;
+    });
+}
