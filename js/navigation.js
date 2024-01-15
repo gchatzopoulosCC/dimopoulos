f35617793e7fc6ff08e219a45876a8caafd51e06
gchatzopoulosCC <gchatzopoulos@york.citycollege.eu>
2024-01-15 10:18:31 +0200

    Finish the navigation, but with the issue of clicking two times for the menu to be shown on mobile

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,16 @@
 // Navigation variables
-let restaurants = [];
-let bars = [];
-let clubs = ["jadore", "suita", "nhaos", "casper", "aigli", "pili axiou"];
+const restaurants = [];
+const restaurants_hrefs = ['#', "#", '#', '#', '#']
+const bars = [];
+const bars_hrefs = ['#', "#", '#', '#', '#', '#']
+const clubs = ["jadore", "suita", "nhaos", "casper", "aigli", "pili axiou"];
+const clubs_hrefs = ['#', '#', "#", '#', '#', '#']
 
 // Temp
-for (let i = 1; i < 5; i++) {
+for (let i = 1; i <= restaurants_hrefs.length; i++) {
     restaurants.push(`restaurant ${i}`);
 }
-for (let i = 1; i < 6; i++) {
+for (let i = 1; i <= clubs_hrefs.length; i++) {
     bars.push(`bar ${i}`);
 }
 
@@ -17,69 +20,60 @@ const dropdowns = document.querySelectorAll(".dropdowns");
 
 // When the user hovers over a menu display the correct content
 dropdowns.forEach(dropdown => {
-    const dropdownDiv = dropdown.querySelector(".dropdown-div");
+    const menu = dropdown.querySelector(".dropdown-menu");
 
-    // On hover
-    dropdown.addEventListener('mouseover', function(){
-        dropdownDiv.style.display = "block";
-    });
-                
-    // Display and hide the menu on and out of hover
-    dropdown.addEventListener('mouseout', function(){
-        dropdownDiv.style.display = "none";
-    });
-
-    // Dynamically add the content to the menu
-    const menu = dropdown.querySelector(".dropdown-menu");        
+    // Dynamically add the content to the menu       
     let data = dropdown.dataset;
     let dropdownInfo;
+    let hrefs;
     // Find the dropdown the user hovers
     switch(data.name) {
         case "restaurants":
             dropdownInfo = restaurants;
+            hrefs = restaurants_hrefs;
             break;
         case "bars":
             dropdownInfo = bars;
+            hrefs = bars_hrefs;
             break;
         case "clubs":
             dropdownInfo = clubs;
+            hrefs = clubs_hrefs;
             break;
         default:
             dropdownInfo = [""];
         }
 
         // Clear before adding content
-        menu.textContent = "";
-        dropdownContent(menu, dropdownInfo);
+        menu.innerHTML = " ";
+        dropdownContent(menu, dropdownInfo, hrefs);
+
+        // Show and hide the menu using the buttons
+        const btn = dropdown.querySelector('.dropdown-buttons');
+        btn.onmouseover = function(){menu.classList.add('show');}
+        btn.onmouseout = function(){menu.classList.remove('show');}
+        
+        btn.onclick = function(){menu.classList.toggle('show');}
+        
+        // Show and hide the menu when the user uses the menu
+        menu.onmouseover = function(){menu.classList.add('show');}
+        menu.onmouseout = function(){menu.classList.remove('show');}
 });
-
-// Dropdown menu rules
-const dropdownDivs = document.querySelectorAll(".dropdown-div");
-dropdownDivs.forEach(div => {
-    div.addEventListener('mouseover', function(){
-        div.style.display = "block";
-    });
-
-    div.addEventListener('click', function(){
-        div.style.display = "none";
-    });
-
-    div.addEventListener('mouseout', function(){
-        div.style.display = "none";
-    });
-});
-
 
 // Burger menu
 const burgerMenu = document.querySelector("#burger-menu");
 const burgerBars = document.querySelectorAll(".burger-bars");
-burgerMenu.addEventListener('click', function(){
+const navList = document.querySelector("#nav-list");
+burgerMenu.onclick = function(){
     burgerBars.forEach(bar => bar.classList.toggle('clicked'));
-});
+    navList.classList.toggle('show');
+};
 
 // Add the content when a dropdown is hovered
-function dropdownContent(menu, dropdownInfo) {
-    dropdownInfo.forEach(content => {
-        menu.innerHTML += `<li class="dropdown-menu-li">${content}</li>`;
-    });
+function dropdownContent(menu, dropdownInfo, hrefs) {
+    for (let i = 0; i < dropdownInfo.length; i++) {
+        menu.innerHTML += `<li class="dropdown-menu-li"><a href="${hrefs[i]}">${dropdownInfo[i]}</a></li>`;
+    }
 }
+
+// Check if the user is on mobile device
