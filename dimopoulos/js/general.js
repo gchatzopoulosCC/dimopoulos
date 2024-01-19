4a9fbde9da6a1f06e42a306be474e6f62e444455
gchatzopoulosCC <gchatzopoulos@york.citycollege.eu>
2024-01-19 23:10:19 +0200

    Merge all the parts, organize and finalize project

--- dimopoulos/js/general.js
+++
@@ -1,8 +0,0 @@
-// Retrieve css variables
-const cssVariables = getComputedStyle(document.body)
-
-// Colors
-export const white = cssVariables.getPropertyValue('white');
-export const lightBlue = cssVariables.getPropertyValue('light_blue');
-export const blue = cssVariables.getPropertyValue('blue');
-export const beige = cssVariables.getPropertyValue('beige');
